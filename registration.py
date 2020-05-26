6f169f6802e2630abe35e8bbf7d941cff27a604e
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2020-05-26 16:01:37 +0200

    Add files via upload

--- registration.py
+++ registration.py
@@ -1,10 +1,11 @@
 
 
-from Conection_database import connection
-from get_string import get_string
+from Conection_database import Connection
+from get_Boolean import get_Boolean
 
 class registration:
     def __init__(self):
+        self.connection= Connection()
         with open("insert_script_waiter.txt", "r") as file:
             self.script_waiter = file.read()
 
@@ -23,13 +24,7 @@ class registration:
                     break
 
     def  finish(self):
-        print("nepodarilo sa skus este raz")
-        again ='skusit znova? Y/N:\n'
-        odpoved = get_string().get_one_string(again)
-        odpoved = odpoved.upper()
-        if odpoved == 'Y':
-            return False
-        return True
+        return not get_Boolean().get("nepodarilo sa skus este raz alebo odid, "+'skusit znova?')
 
     def regist_cook(self):
         while True:
@@ -45,8 +40,5 @@ class registration:
     def regist(self, script):
         apply_name, apply_password='zadaj meno:\n',"zadaj heslo:\n"
         name_password = get_string().get_string_and_sifr(apply_name, apply_password)
-        connection.execute(script, tuple(name_password))        
+        self.connection.execute(script, tuple(name_password))       
 
-r =registration()
-r.regist_waiter()
-r.regist_cook()
